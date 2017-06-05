d3c289e9a6a08c30c00ce204de83c06e5ae85343
Chris Anderson <gca2@earthlink.net>
2017-06-05 05:48:14 +0000

    Finished refactoring for async

--- Asynclidar_drive.py
+++ Asynclidar_drive.py
@@ -9,11 +9,12 @@ from rplidar import RPLidar
 import PyCmdMessenger
 import math
 
-angleoffset = 45
+angle_offset = 45
 gain = 2
 speed = 70
 left = 0
 right = 0
+start = time.time()
 
 PORT_NAME = '/dev/ttyUSB0' # this is for the Lidar
 
@@ -38,27 +39,50 @@ c = PyCmdMessenger.CmdMessenger(myarduino,commands)
 def tic():
     return 'at %1.1f seconds' % (time.time() - start)
 
-async def gr1():
-    # Busy waits for a tenth of a second, but we don't want to stick around...
+async def scan(lidar):
     print('10Hz loop started work: {}'.format(tic()))
+    global average_angle
     time1 = time.time()
     while True:
-      # do some work here
-      await asyncio.sleep(0)
-      if time.time() > time1 + 0.1:
-        print('10Hz loop ended work: {}'.format(tic()))
-        time1 = time.time()
+        counter = 0
+        print('Recording measurements... Press Crl+C to stop.')
+        data = 0
+        lasttime = time.time()
+        for measurment in lidar.iter_measurments():
+            await asyncio.sleep(0)  # this allows other threads to run
+            if (measurment[2] > 0 and measurment[2] < 90):  # in angular range
+                if (measurment[3] < 1000 and measurment[3] > 100): # in distance range
+                    data = data + measurment[2] # angle
+                    counter = counter + 1 # increment counter
+            if time.time() > (lasttime + 0.1):
+                print("this should happen ten times a second")
+                if counter > 0:  # this means we see something
+                    average_angle = data/counter
+                    print ("Average Angle: ", average_angle-angle_offset)
+                    counter = 0
+                lasttime = time.time()  # reset 10Hz timer
 
-async def gr2():
-    # Busy waits for a second, but we don't want to stick around...
-    print('1 Hz loop started work: {}'.format(tic()))
-    time2 = time.time()
-    while True:
-      # do some work here
-      await asyncio.sleep(0)
-      if time.time() > time2 + 1:
-        print('1 Hz loop ended work: {}'.format(tic()))
+async def drive():
+    global speed
+    global gain
+    global angle_offset
+    global average_angle
+    if time.time() > time2 + 0.1: # run this ten times a second 
+        # Send motor commands
+        steer = int(100*math.atan(math.radians(average_angle-angle_offset)))
+        print ("steer: ", steer * gain)
+        if steer <= 0:
+            right = -1 * steer * gain
+            left = 0
+        else:
+            left = steer * gain
+            right = 0
+        print (speed, left, right) 
+        c.send("motors",speed,left,right)
+        print('Drive command sent: {}'.format(tic()))
         time2 = time.time()
+    await asyncio.sleep(0)  # go back to running other threads
+        
 
 async def gr3():
     print("Let's do some stuff while the coroutines are blocked, {}".format(tic()))
@@ -69,64 +93,29 @@ async def gr3():
         print("Done!")
       await asyncio.sleep(0)
 
-def drive(angle):
-    global speed
-    global gain
-    # Send motor commands
-    steer = int(100*math.atan(math.radians(angle)))
-    print ("steer: ", steer * gain)
-    if steer <= 0:
-        right = -1 * steer * gain
-        left = 0
-    else:
-        left = steer * gain
-        right = 0
-    print (speed, left, right) 
-    c.send("motors",speed,left,right)
-
-def scan(lidar):
+def run():
+    '''Main function'''
+    lidar = RPLidar(PORT_NAME)
+    lidar.start_motor()
+    time.sleep(1)
+    info = lidar.get_info()
+    print(info)
+    ioloop = asyncio.get_event_loop()
+    tasks = [
+    	    ioloop.create_task(scan(lidar)),
+    	    ioloop.create_task(drive()),
+    	    ioloop.create_task(gr3())
+	    ]
+    ioloop.run_until_complete(asyncio.wait(tasks))
+    ioloop.close()
     try:
-        counter = 0
-        print('Recording measurements... Press Crl+C to stop.')
-        data = 0
-        lasttime = time.time()
-        for measurment in lidar.iter_measurments():
-            if (measurment[2] > 0 and measurment[2] < 90):  # in angular range
-                if (measurment[3] < 1000 and measurment[3] > 100): # in distance range
-                    data = data + measurment[2] # angle
-                    counter = counter + 1 # increment counter
-            if time.time() > (lasttime + 0.1):
-                print("this should happen ten times a second")
-                if counter > 0:  # this means we see something
-                    average_angle = data/counter
-                    print ("Average Angle: ", average_angle-angleoffset)
-#                    drive (average_angle-angleoffset)
-                lasttime = time.time()  # reset 10Hz timer
+        i =0
     except KeyboardInterrupt:
         print('Stopping.')
         lidar.stop()
         lidar.stop_motor()
         c.send("motors",0,0,0)  # turn off wheel motors
         lidar.disconnect()
-
-def run():
-    '''Main function'''
-    drive (0) # start
-    lidar = RPLidar(PORT_NAME)
-    lidar.start_motor()
-    time.sleep(1)
-    info = lidar.get_info()
-    print(info)
-#    ioloop = asyncio.get_event_loop()
-#    tasks = [
-#    	    ioloop.create_task(gr1()),
-#    	    ioloop.create_task(gr2()),
-#    	    ioloop.create_task(gr3())
-#	    ]
-#    ioloop.run_until_complete(asyncio.wait(tasks))
-#    ioloop.close()
-    while True:
-        scan(lidar)
  
 if __name__ == '__main__':
     run()
